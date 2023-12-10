dfc77e72a9ff3a686246afd6cbcee711f87a704b
Antek <tonylearnscoding@gmail.com>
2023-12-10 14:20:17 +0100

    [LR-3] Create User entity, resolver & migration

---
+++ server/src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, Property, PrimaryKey } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+@Entity()
+export class User {
+  @Field()
+  @PrimaryKey()
+  id!: number;
+
+  @Field()
+  @Property()
+  createdAt: Date = new Date();
+
+  @Field()
+  @Property({ onUpdate: () => new Date() })
+  updatedAt: Date = new Date();
+
+  @Field()
+  @Property({ type: 'text', unique: true })
+  username: string;
+
+  @Property({ type: 'text' })
+  password: string;
+}
